d8815c5aab4cc3dc09ac2c1d17aaad931a94a104
José Manuel Sauri Chavez <saurichavez@unikino.edu.mx>
2018-07-07 15:03:10 -0700

    07/07/18 Practica Clase

--- ProgramacionOrientadaObjetosII/ProgramacionOrientadaObjetosII/Form1.cs
+++ ProgramacionOrientadaObjetosII/ProgramacionOrientadaObjetosII/Form1.cs
@@ -13,6 +13,7 @@ namespace ProgramacionOrientadaObjetosII
 	public partial class Form1 : Form
 	{
 		private List<Productos> listaproductos = new List<Productos>();
+		Productos[] producto = new Productos[3];
 		public Form1()
 		{
 			InitializeComponent();
@@ -25,27 +26,49 @@ namespace ProgramacionOrientadaObjetosII
 			productos.producto_nombre = "MILLER";
 			productos.producto_precio = 36.00f;
 
-			richTextBox1.Text += productos.producto_codigo + productos.producto_nombre + productos.producto_precio;*/		
-			listaproductos.Add();
-			//tes
+			richTextBox1.Text += productos.producto_codigo + productos.producto_nombre + productos.producto_precio;*/
+
+
+			producto[0] = new Productos(1, "producto 1", 35.00f);
+			producto[1] = new Productos(2, "producto 2", 35.02f);
+			producto[2] = new Productos(3, "producto 3", 35.08f);
+			cargaProducto();
+
 		}
-
-		private void cargaProductos()
+		private void cargaProducto()
 		{
+			dataGridView1.Columns[2].DefaultCellStyle.Format = "N2";
+			try
+			{
+				for (int i = 0; i < producto.Length; i++)
+				{
+					dataGridView1.Rows.Add(producto[i].pCodigo, producto[i].pNombre, producto[i].pPrecio);
+				}
+			}
+			catch (Exception err)
+			{
 
+				MessageBox.Show("ERROR:" + Environment.NewLine + err);
+			}
 		}
 	}
-
-	public class Productos
+	class Productos
 	{
 		//Propiedades
-		public long producto_codigo = 0;
-		public String producto_nombre = "";
-		public float producto_precio = 0.00f;
+		public long pCodigo = 0;
+		public String pNombre = "";
+		public float pPrecio = 0.00f;
 
 		public Productos()
 		{
-
+			MessageBox.Show("Me llamaron sin parametros");
+		}
+		public Productos(long pCodigo, String pNombre, float pPrecio)
+		{
+			this.pCodigo = pCodigo;
+			this.pNombre = pNombre;
+			this.pPrecio = pPrecio;
 		}
 	}
-}
+
+}	
