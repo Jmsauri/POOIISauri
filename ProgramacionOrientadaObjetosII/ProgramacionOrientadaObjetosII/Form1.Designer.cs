f8b7db0539db05896455cad7ba445e83b5dfa911
José Manuel Sauri Chavez <saurichavez@unikino.edu.mx>
2018-07-14 15:01:30 -0700

    Practica 14/07/2018

--- ProgramacionOrientadaObjetosII/ProgramacionOrientadaObjetosII/Form1.Designer.cs
+++ ProgramacionOrientadaObjetosII/ProgramacionOrientadaObjetosII/Form1.Designer.cs
@@ -33,56 +33,165 @@
 			this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
 			this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
 			this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.IdProducto = new System.Windows.Forms.TextBox();
+			this.NProducto = new System.Windows.Forms.TextBox();
+			this.Precio = new System.Windows.Forms.TextBox();
+			this.button3 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.BtnAdd = new System.Windows.Forms.Button();
 			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
 			this.SuspendLayout();
 			// 
 			// richTextBox1
 			// 
-			this.richTextBox1.Location = new System.Drawing.Point(13, 13);
+			this.richTextBox1.Location = new System.Drawing.Point(770, 3);
 			this.richTextBox1.Name = "richTextBox1";
-			this.richTextBox1.Size = new System.Drawing.Size(775, 204);
+			this.richTextBox1.Size = new System.Drawing.Size(18, 13);
 			this.richTextBox1.TabIndex = 0;
 			this.richTextBox1.Text = "";
 			// 
 			// dataGridView1
 			// 
+			this.dataGridView1.AllowUserToAddRows = false;
+			this.dataGridView1.AllowUserToDeleteRows = false;
 			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
 			this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.Column1,
             this.Column2,
             this.Column3});
-			this.dataGridView1.Location = new System.Drawing.Point(13, 253);
+			this.dataGridView1.Location = new System.Drawing.Point(12, 12);
 			this.dataGridView1.Name = "dataGridView1";
-			this.dataGridView1.Size = new System.Drawing.Size(546, 185);
+			this.dataGridView1.ReadOnly = true;
+			this.dataGridView1.Size = new System.Drawing.Size(351, 426);
 			this.dataGridView1.TabIndex = 1;
+			this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
 			// 
 			// Column1
 			// 
-			this.Column1.HeaderText = "Column1";
+			this.Column1.HeaderText = "Codigo";
 			this.Column1.Name = "Column1";
+			this.Column1.ReadOnly = true;
 			// 
 			// Column2
 			// 
-			this.Column2.HeaderText = "Column2";
+			this.Column2.HeaderText = "Producto";
 			this.Column2.Name = "Column2";
+			this.Column2.ReadOnly = true;
 			// 
 			// Column3
 			// 
-			this.Column3.HeaderText = "Column3";
+			this.Column3.HeaderText = "Precio";
 			this.Column3.Name = "Column3";
+			this.Column3.ReadOnly = true;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(369, 37);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(62, 13);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "Id Producto";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(369, 82);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(44, 13);
+			this.label2.TabIndex = 3;
+			this.label2.Text = "Nombre";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(369, 133);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(37, 13);
+			this.label3.TabIndex = 4;
+			this.label3.Text = "Precio";
+			// 
+			// IdProducto
+			// 
+			this.IdProducto.Cursor = System.Windows.Forms.Cursors.AppStarting;
+			this.IdProducto.Location = new System.Drawing.Point(446, 30);
+			this.IdProducto.Name = "IdProducto";
+			this.IdProducto.Size = new System.Drawing.Size(100, 20);
+			this.IdProducto.TabIndex = 5;
+			// 
+			// NProducto
+			// 
+			this.NProducto.Location = new System.Drawing.Point(446, 79);
+			this.NProducto.Name = "NProducto";
+			this.NProducto.Size = new System.Drawing.Size(100, 20);
+			this.NProducto.TabIndex = 6;
+			// 
+			// Precio
+			// 
+			this.Precio.Location = new System.Drawing.Point(446, 130);
+			this.Precio.Name = "Precio";
+			this.Precio.Size = new System.Drawing.Size(100, 20);
+			this.Precio.TabIndex = 7;
+			// 
+			// button3
+			// 
+			this.button3.BackgroundImage = global::ProgramacionOrientadaObjetosII.Properties.Resources.if_view_refresh_118801;
+			this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.button3.Location = new System.Drawing.Point(572, 116);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(50, 46);
+			this.button3.TabIndex = 10;
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.button3_Click);
+			// 
+			// button2
+			// 
+			this.button2.BackgroundImage = global::ProgramacionOrientadaObjetosII.Properties.Resources.if_f_cross_256_282471;
+			this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.button2.Location = new System.Drawing.Point(572, 67);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(50, 43);
+			this.button2.TabIndex = 9;
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// BtnAdd
+			// 
+			this.BtnAdd.BackgroundImage = global::ProgramacionOrientadaObjetosII.Properties.Resources.if_sign_add_299068;
+			this.BtnAdd.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.BtnAdd.Location = new System.Drawing.Point(572, 18);
+			this.BtnAdd.Name = "BtnAdd";
+			this.BtnAdd.Size = new System.Drawing.Size(50, 43);
+			this.BtnAdd.TabIndex = 8;
+			this.BtnAdd.UseVisualStyleBackColor = true;
+			this.BtnAdd.Click += new System.EventHandler(this.BtnAdd_Click);
 			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.BtnAdd);
+			this.Controls.Add(this.Precio);
+			this.Controls.Add(this.NProducto);
+			this.Controls.Add(this.IdProducto);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
 			this.Controls.Add(this.dataGridView1);
 			this.Controls.Add(this.richTextBox1);
 			this.Name = "Form1";
 			this.Text = "Form1";
+			this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
 			this.Load += new System.EventHandler(this.Form1_Load);
 			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
@@ -93,6 +202,15 @@
 		private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
 		private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
 		private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.TextBox IdProducto;
+		private System.Windows.Forms.TextBox NProducto;
+		private System.Windows.Forms.TextBox Precio;
+		private System.Windows.Forms.Button BtnAdd;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button3;
 	}
 }
 
--- ProgramacionOrientadaObjetosII/ProgramacionOrientadaObjetosII/Form1.cs
+++ ProgramacionOrientadaObjetosII/ProgramacionOrientadaObjetosII/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace ProgramacionOrientadaObjetosII
 {
@@ -14,6 +15,8 @@ namespace ProgramacionOrientadaObjetosII
 	{
 		private List<Productos> listaproductos = new List<Productos>();
 		Productos[] producto = new Productos[3];
+		private int rowSelect;
+
 		public Form1()
 		{
 			InitializeComponent();
@@ -21,35 +24,118 @@ namespace ProgramacionOrientadaObjetosII
 
 		private void Form1_Load(object sender, EventArgs e)
 		{
-			/*Productos productos = new Productos();
-			productos.producto_codigo = 12345698764;
-			productos.producto_nombre = "MILLER";
-			productos.producto_precio = 36.00f;
-
-			richTextBox1.Text += productos.producto_codigo + productos.producto_nombre + productos.producto_precio;*/
-
-
 			producto[0] = new Productos(1, "producto 1", 35.00f);
 			producto[1] = new Productos(2, "producto 2", 35.02f);
 			producto[2] = new Productos(3, "producto 3", 35.08f);
 			cargaProducto();
 
 		}
+
 		private void cargaProducto()
 		{
 			dataGridView1.Columns[2].DefaultCellStyle.Format = "N2";
+
 			try
 			{
-				for (int i = 0; i < producto.Length; i++)
+				int counter = 0;
+				string line;
+				System.IO.StreamReader file =
+				new System.IO.StreamReader(@"C:\Users\JmSaurii\source\repos\NewRepo2\ProgramacionOrientadaObjetosII\ProgramacionOrientadaObjetosII\bin\Debug\practica.csv");
+				while ((line = file.ReadLine()) != null)
 				{
-					dataGridView1.Rows.Add(producto[i].pCodigo, producto[i].pNombre, producto[i].pPrecio);
+					dataGridView1.Text += line + Environment.NewLine;
+					dataGridView1.Rows.Add(line.Split(','));
+					dataGridView1.Rows[counter].HeaderCell.Value = (counter + 1).ToString();
+					counter++;
 				}
+				file.Close();
 			}
-			catch (Exception err)
+			catch (Exception error)
 			{
-
-				MessageBox.Show("ERROR:" + Environment.NewLine + err);
+				MessageBox.Show("No sale" + error);
 			}
+		}
+
+		private void BtnAdd_Click(object sender, EventArgs e)
+		{
+			bool esta = false;
+			for (int i = 0; i < dataGridView1.Rows.Count; i++)
+			{
+				if (dataGridView1[0, i].Value.ToString() == IdProducto.Text)
+				{
+					esta = true;
+				}
+				
+			}
+
+			if (!esta)
+			{
+				dataGridView1.Rows.Add(IdProducto.Text, NProducto.Text, Precio.Text);
+				dataGridView1_Numerar();
+			}
+
+			else
+			{
+				MessageBox.Show("Producto Ya Existe");
+			}
+
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			if (MessageBox.Show("Desea Eliminar" + dataGridView1[1, rowSelect].Value.ToString() + "Precio:" + dataGridView1[2, rowSelect].Value.ToString(), "Eliminar Producto", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+			{
+				dataGridView1.Rows.RemoveAt(rowSelect);
+			}
+			limpiar();
+		}
+
+		private void limpiar()
+		{
+			IdProducto.Clear();
+			NProducto.Clear();
+			Precio.Clear();
+			IdProducto.Focus();
+		}
+
+		private void dataGridView1_Numerar()
+		{
+			for (int i = 0; i < dataGridView1.Rows.Count; i++)
+			{
+				dataGridView1.Rows[i].HeaderCell.Value = (i + 1).ToString();
+			}
+        }
+
+		private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+		{
+			if (e.RowIndex != -1)
+			{
+				rowSelect = e.RowIndex;
+				IdProducto.Text = dataGridView1[0, rowSelect].Value.ToString();
+				NProducto.Text = dataGridView1[1, rowSelect].Value.ToString();
+				Precio.Text = dataGridView1[2, rowSelect].Value.ToString();
+				rowSelect = e.RowIndex;
+				IdProducto.Enabled = false;
+			}
+		}
+
+		private void button3_Click(object sender, EventArgs e)
+		{
+			if (MessageBox.Show("Desea Modificar" + dataGridView1[1, rowSelect].Value.ToString() + "Precio:" + dataGridView1[2, rowSelect].Value.ToString(), "Eliminar Producto", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+			{
+				dataGridView1[1, rowSelect].Value = NProducto.Text;
+				dataGridView1[2, rowSelect].Value = Precio.Text;
+			}
+			limpiar();
+		}
+
+		private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+		{
+			MessageBox.Show("Verificar que se guardo");
+
+			dataGridView1.ClipboardCopyMode = DataGridViewClipboardCopyMode.EnableWithoutHeaderText;
+			DataObject dataObject = dataGridView1.GetClipboardContent();
+			File.WriteAllText("practica.csv", dataObject.GetText(TextDataFormat.CommaSeparatedValue));
 		}
 	}
 	class Productos
